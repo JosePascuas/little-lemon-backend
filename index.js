4bfeaf39cb4fda00ba2ad5015601905589878227
Jose Pascuas <josemi0@hotmail.com>
2025-05-26 23:20:36 -0500

    fixed

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ app.post('/reservas', (req, res) => {
   const { nombre, numero, fecha, hora, personas, ocasion } = req.body;
   console.log("Datos recibidos en backend:", req.body);
 
+  const parsedDate = new Date(fecha);
+  parsedDate.setMinutes(parsedDate.getMinutes() + parsedDate.getTimezoneOffset());
+  const fechaAjustada = parsedDate.toISOString().split("T")[0];
+  fecha = fechaAjustada;
+
   const query = 'INSERT INTO reservas (nombre, numero, fecha, hora, personas, ocasion) VALUES (?, ?, ?, ?, ?, ?)';
 
   db.query(query, [nombre, numero, fecha, hora, personas, ocasion], (err, result) => {
