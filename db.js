9165d5c219f1f029343f0eab262184983470f7cd
Jose Pascuas <josemi0@hotmail.com>
2025-05-26 20:16:03 -0500

    guardando

--- db.js
+++ db.js
@@ -12,12 +12,4 @@ const db = mysql.createPool({
   queueLimit: 0,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('❌ Error al conectar a la base de datos:', err);
-  } else {
-    console.log('✅ Conectado a la base de datos MySQL');
-  }
-});
-
 module.exports = db;
